50489d58386427826ca11cb3efbca9d4b4dbb311
Drew Baumann <drewbaumann@gmail.com>
2012-02-18 16:50:38 -0800

    port ready for heroku

--- app.js
+++ app.js
@@ -49,6 +49,7 @@ everyauth.helpExpress(app);
 
 app.get('/id', id.id);
 
+var port = process.env.PORT || 3000;
 
-app.listen(3000);
+app.listen(port);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
