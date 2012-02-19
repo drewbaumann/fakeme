726907a1ea6cd8cab7485c34caebe9f138ef1954
Drew Baumann <drewbaumann@gmail.com>
2012-02-18 16:09:35 -0800

    regenerates new info on page load. added a male first name list. fixed the faker us_state method

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,12 @@
 /*
  * GET home page.
  */
-
 exports.index = function(req, res){
-  res.render('index', { title: 'The Fake me' })
+  var Faker = require('Faker');
+  var first_name = Faker.Name.maleFirstName();
+  var last_name = Faker.Name.lastName();
+  var city = Faker.Address.city();
+  var state = Faker.Address.usState();
+  var company = Faker.Company.companyName();
+  res.render('index', { title: 'The Fake me', first_name: first_name, last_name: last_name, city: city, state: state, company: company  })
 };
